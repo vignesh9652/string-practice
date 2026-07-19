b7d65eb4d84b1084cbe21256546f71f876c16aa7
vignesh9652 <panchavativignesh@gmail.com>
2026-07-19 23:02:11 +0530

    Count Charcter

---
+++ strings/src/CountCharcter.java
@@ -0,0 +1,18 @@
+public class CountCharcter {
+    public static void main(String[] args) {
+
+        String str="Java Full Stack";
+
+        int count=0;
+
+        for(int i=0;i<str.length();i++){
+
+            if(str.charAt(i)!=' ')
+                count++;
+
+        }
+
+        System.out.println(count);
+
+    }
+}
