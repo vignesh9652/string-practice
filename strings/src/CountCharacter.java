b9609c338586b4cbb83a7e2006e05f122441920d
vignesh9652 <panchavativignesh@gmail.com>
2026-07-20 21:52:33 +0530

    Count Consonants

--- strings/src/CountCharacter.java
+++ strings/src/CountCharacter.java
@@ -1,4 +1,4 @@
-public class CountCharcter {
+public class CountCharacter {
     public static void main(String[] args) {
 
         String str="Java Full Stack";
---
+++ strings/src/CountConsonants.java
@@ -0,0 +1,28 @@
+public class CountConsonants {
+
+    public static void main(String[] args) {
+
+        String str = "Programming";
+
+        int count = 0;
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+
+                if (!(ch == 'a' || ch == 'e' || ch == 'i'
+                        || ch == 'o' || ch == 'u')) {
+
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Total Consonants : " + count);
+
+    }
+}
